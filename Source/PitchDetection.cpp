597470cf03c33993aa3aee5a51c49c1f8b17ed31
jorshi <jordieshier@gmail.com>
2017-03-22 11:47:48 -0700

    New class for running pitch detection

---
+++ Source/PitchDetection.cpp
@@ -0,0 +1,21 @@
+/*
+  ==============================================================================
+
+    PitchDetection.cpp
+    Created: 22 Mar 2017 11:41:06am
+    Author:  Jordie Shier 
+
+  ==============================================================================
+*/
+
+#include "PitchDetection.h"
+
+
+PitchDetection::PitchDetection()
+{
+}
+
+
+PitchDetection::~PitchDetection()
+{
+}
---
+++ Source/PitchDetection.h
@@ -0,0 +1,32 @@
+/*
+  ==============================================================================
+
+    PitchDetection.h
+    Created: 22 Mar 2017 11:41:06am
+    Author:  Jordie Shier 
+
+  ==============================================================================
+*/
+
+#ifndef PITCHDETECTION_H_INCLUDED
+#define PITCHDETECTION_H_INCLUDED
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+class PitchDetection
+{
+public:
+    
+    //Default Constructor
+    PitchDetection();
+    
+    // Deconstructor
+    ~PitchDetection();
+    
+private:
+    
+};
+
+
+
+#endif  // PITCHDETECTION_H_INCLUDED
